7dda27aac85435af7921b58c8be5e4104a3ad024
Aaron-vk-Yuan <xuanyuanziruo@gmail.com>
2017-04-12 18:22:57 +0800

    add

---
+++ src/views/about/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h2>Welcome</h2>
+      <div>About page</div>
+      <p>
+        this from about
+      </p>
+    </div>
+  );
+}
---
+++ src/views/product/index.jsx
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import connect from 'store/connect';
+import Header from 'commonComponents/header';
+import { Button } from 'antd';
+
+import List from '../../components/list';
+
+
+class Product extends Component {
+  constructor() {
+    super();
+    this.state = Object.assign({}, {count: 0});
+  }
+  componentWillMount() {
+    this.setState({
+      count: this.state.count + 1,
+      abc: 123
+    });
+  }
+  render() {
+    console.log('product', this);
+    this.renderList();
+    return (
+      <div>
+        <Header />
+        <List/>
+        <Button type="primary">this is a button</Button>
+        product list
+        <hr/> {this.props.user.basicInfo.name || 'test'}
+      </div>
+    );
+  }
+  renderList = () => {}
+}
+
+export default connect((state) => {
+  return {user: state.user};
+}, Product);
